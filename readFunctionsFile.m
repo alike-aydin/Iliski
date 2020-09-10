1b4468fa8511248939fe3c1c05da017ab4c07304
Ali-Kémal Aydin <ali-kemal.aydin@inserm.fr>
2020-09-10 15:48:51 +0200

    Add Feature
    Feature from #20 to change the default parameters for parametric functions

--- readFunctionsFile.m
+++ readFunctionsFile.m
@@ -1,5 +1,5 @@
-function [fctNames,fctHandles] = readFunctionsFile(filename)
-%function [fctNames,fctHandles] = readFunctionsFile(filename)
+function [fctNames,fctHandles, defParams] = readFunctionsFile(filename)
+%function [fctNames,fctHandles, defParams] = readFunctionsFile(filename)
 %
 %   Author : Ali-Kemal Aydin, PhD student
 %   Date : May 28th, 2020
@@ -15,7 +15,7 @@ function [fctNames,fctHandles] = readFunctionsFile(filename)
 %   The file has the following properties:
 %       - named 'DefaultParametricFunctions_Iliski.txt' (first parameter);
 %       - one function per line, without any header;
-%       - FUNCTION NAME: HANDLE
+%       - FUNCTION NAME: HANDLE: DEFAULT PARAMETERS
 %           * The name of the function is everything that comes before the
 %           colon. It can be spaced and with special characters, it is
 %           meant to be displayed in the GUI.
@@ -23,25 +23,30 @@ function [fctNames,fctHandles] = readFunctionsFile(filename)
 %           any limitations on the number of parameters. The order and
 %           names of the parameters will be kept inside the GUI. The last
 %           parameter is _always_ the time.
+%           * Default parameters are written like an array of numbers and
+%           will be automatically entered as the starting parameters for
+%           the optimzation.
 %           * See below for an example.
 %
 %   EXAMPLE of file content:
-%       1-Gamma: @(p1, p2, p3, p4, t) heaviside(t-p3).*p4.*(t-p3).^(p1-1).*(p2^p1).*exp(-p2.*(t-p3))./(gamma(p1))
-%       2-Logit: @
+%       1-Gamma: @(p1, p2, p3, p4, t)((t-p3)>=0).*p4.*(t-p3).^(p1-1).*(p2^p1).*exp(-p2.*(t-p3))./(gamma(p1)):[1.3 0.5 0.27 0.19]
+%       2-Inverse Logit: @(p1, p2, p3, p4, p5, p6, t)p5*(1./(1+exp(-((t-p1)/p3)))) - p6*(1./(1+exp(-((t-p2)/p4)))):[2 2 2 2 2 2]
 %__________________________________________________________________________
 %   PARAMETERS:
-%       filename (str) : name and path if needed to the file.
+%       filename (str): name and path if needed to the file.
 %
 %__________________________________________________________________________
 %   RETURN:
-%       fctNames ({}) : cell array of the functions name.
+%       fctNames ({}): cell array of the functions name.
 %
-%       fctHandles ({}) : cell array of the functions handle.
+%       fctHandles ({}): cell array of the functions handle.
+%
+%       defParams ([]): 2D array of the default parameters.
 %__________________________________________________________________________
 
 try
     fileID = fopen(filename);
-    content = textscan(fileID, '%s%s', 'Delimiter', {':'}, 'CollectOutput', 1);
+    content = textscan(fileID, '%s%s%s', 'Delimiter', {':'}, 'CollectOutput', 1);
     fclose(fileID);
 catch ME
     msg = ['Error while reading the functions file: ', filename,'.'];
@@ -54,6 +59,8 @@ content = content{1};
 nbFcts = size(content, 1);
 fctNames = {};
 fctHandles = {};
+defParamsStr = {};
+defParams = {};
 
 for i=1:nbFcts
     fctNames{i} = content{i, 1};
@@ -61,6 +68,17 @@ for i=1:nbFcts
         fctHandles{i} = str2func(content{i, 2});
     catch ME
         msg = ['Error while creating the function handle, in line ', ...
+            num2str(i), ' of ', filename, '.'];
+        causeException = MException(...
+            'Iliski:ParametricFunctions:ParsingHandleError', msg);
+        rethrow(addCause(ME, causeException));
+    end
+    
+    defParamsStr{i} = content{i, 3};
+    try
+        defParams{i} = str2num(defParamsStr{i});
+    catch
+        msg = ['Error while reading the default parameters for the function handle, in line ', ...
             num2str(i), ' of ', filename, '.'];
         causeException = MException(...
             'Iliski:ParametricFunctions:ParsingHandleError', msg);
